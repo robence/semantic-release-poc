ccdce3b4d49e14265888ddf302b36bdfe5ef3eba
robence <robence@gmail.com>
2023-06-20 18:40:38 +0200

    fix: KKNEXT-1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('BREAKING CHANGE! some customizaitons');
+console.log('BREAKING CHANGE! some customizaitons kk next');
 
 console.log('BREAKING CHANGE 4 test');
 console.log('change 4');
