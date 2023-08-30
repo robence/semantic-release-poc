fa9dec3ce3659472000aa38296f95933d98ef1b0
robence <robence@gmail.com>
2023-08-30 15:17:35 +0200

    build(ci): increase commit footer max line length

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,6 +7,7 @@ module.exports = {
       "always", // example: feat: FKDEV-1 (#2)
       ["sentence-case", "start-case", "pascal-case", "upper-case"],
     ],
-    'body-max-line-length': [2, 'always', 250]
+    'body-max-line-length': [2, 'always', 250],
+    'footer-max-line-length': [2, 'always', 250]
   },
 };
