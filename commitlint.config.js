e56c92a4a42a0168c9878d1e4abdc880e9170871
robence <robence@gmail.com>
2023-09-05 12:57:09 +0200

    fix(commitlint): remove strict rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    "subject-case": [
-      2,
-      // 'never',
-      "always", // example: feat: FKDEV-1 (#2)
-      ["sentence-case", "start-case", "pascal-case", "upper-case"],
-    ],
-    'body-max-line-length': [2, 'always', 250],
-    'footer-max-line-length': [2, 'always', 2500]
-  },
 };
